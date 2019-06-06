7bc7c909707f0e25c4cb6cfbc1f818d28ff4228c
Krygier <krygich@richmond.k12.ga.us>
2019-06-06 07:06:27 -0400

    Updated Help in the Copy-RCSSFile function.

--- Copy-RCSSFile.ps1
+++ Copy-RCSSFile.ps1
@@ -2,6 +2,33 @@ function Copy-RCSSFile {
     <#
     .SYNOPSIS
     Copy files from a given path to a remote computer.
+
+    .DESCRIPTION
+    The purpose of the script is to copy files from main computer to remote computers. The user will need admin access to create the connection to the remote computer as well add the folder if not present. I had to use the $using: variable as the $destinationfolder variable was not passing to the remote computers. Then, I had to add the variable again into the try block as I learned that the $using: variable only passes the local variable into one command and not recursively. 
+    
+    .PARAMETER computername
+    The remote computer you wish to send files to.
+
+    .PARAMETER path
+    The location of which the file you want to transfer.
+
+    .PARAMETER destination
+    The file path on the remote computer
+
+
+    .PARAMETER destinationfolder
+    The folder path on the remote computer in which you want to place the file.
+
+    .EXAMPLE
+    "Server1","Server2" | Copy-RCSSFile -path "\\Server3\D$\Software\Applications\Shoretel Communicator\setup.exe" -destination "C:\Install\_Code\setup.exe" -Verbose
+
+    IN this example, we are sending computernames into the pipeline and is fed into the Copy-RCSSFile function. This will copy the file in the path paramater into the remote computer destination folder.
+
+    .EXAMPLE
+    Copy-RCSSFile -computername "Server1","Server2" -path "\\Server3\D$\Software\Applications\Shoretel Communicator\setup.exe" -destination "C:\Install\_Code\setup.exe" -Verbose
+
+    Using the whole function to pass the paramaters. Sends the path file to the remote computer(computername) in the destination folder.
+
     #>    
     
         [CmdletBinding()]
